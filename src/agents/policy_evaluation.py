e8aa2e15772f70fff344be04fdce678188ccb6ff
rlins <ramon_asl@yahoo.com.br>
2023-06-22 11:53:20 -0300

    Add convergence criterion
    It also add a docstring information about reference and how to test code.

--- src/agents/policy_evaluation.py
+++ src/agents/policy_evaluation.py
@@ -1,11 +1,22 @@
 """
 This code is a implementation of the policy evaluation algorithm based on stanford-cs221 course.
 
-The enviroment was defined in /src/envs/python/dice_game.py
-
 The lecture was taken from:
 https://www.youtube.com/watch?v=9g32v7bK3Co&list=PLoROMvodv4rOca_Ovz1DvdtWuz8BfSWL2&index=20&t=140s
+
+The enviroment was defined in /src/envs/python/dice_game.py
+
+To test run:
+    from src.envs.python.dice_game import DiceGame
+    from src.agents.policy_evaluation import PolicyEvaluation
+
+    mdp = DiceGame()
+    pol_eva = PolicyEvaluation(mdp)
+
+    print(pol_eva.update("stay"))
 """
+
+
 class PolicyEvaluation:
     def __init__(self, mdp):
         self.t_hash = mdp.t_hash
@@ -26,10 +37,11 @@ class PolicyEvaluation:
                 Vpi(s) = Qpi(s,a) = SUM_s' T(s, a, s') * [R(s, a, s') + gamma * Vpi(s)]
         """
         # design parameters
-        gamma = 1
+        gamma = 0.999
 
-        # TODO: Iterate until converge
-        for _ in range(max_it):
+        # iterate until converge
+        is_iterate = True
+        while is_iterate:
             # go throught all states
             for s in self.states:
                 # avoid undefined state-action in mdp
@@ -40,6 +52,12 @@ class PolicyEvaluation:
                     for next_s in all_next_states:
                         p_t, r = self.t_hash[s, policy][next_s]
                         Vf += p_t * (r + gamma * self.V[next_s])
+
+                    # current V(in) - previous V(in)
+                    e = Vf - self.V[s]
+
+                    if e < 1e-5:
+                        is_iterate = False
 
                     self.V[s] = Vf
 
