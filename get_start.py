df6bea0d70e9c64bc454d1b38679302d54d68b0b
rlins <ramon_asl@yahoo.com.br>
2023-06-24 11:54:20 -0300

    Add q-learning algorithm
    Update get_start.py script to use q-learning algorithm

--- get_start.py
+++ get_start.py
@@ -1,14 +1,25 @@
 import gymnasium as gym
+from src.agents.qlearning import QL
 
-env = gym.make("src:src/GridWorld-v0", render_mode="human")
+env = gym.make("src:src/GridWorld-v0", render_mode="human", size=4)
+agent = QL()
+print(agent)
 
 observation, info = env.reset()
-
+i = 0
 while True:
-    action = env.action_space.sample()
-    observation, reward, terminated, truncated, info = env.step(action)
+    action = agent.action(observation['agent'])
+    next_observation, reward, terminated, truncated, info = env.step(action)
+
+    agent.update(observation['agent'], action, reward, next_observation['agent'])
+
+    observation = next_observation
 
     if terminated or truncated:
         observation, info = env.reset()
+        print(f"Time spent: {i}")
+        i = 0
+
+    i += 1
 
 env.close()
